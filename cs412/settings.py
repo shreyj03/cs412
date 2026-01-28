537b585ca7d3480c6af2677f7d25109e49f83b23
shreyj03 <jainshrey2004@gmail.com>
2026-01-27 19:40:49 -0500

    Added all files for deployment.

--- cs412/settings.py
+++ cs412/settings.py
@@ -139,3 +139,5 @@ if socket.gethostname() == CS_DEPLOYMENT_HOSTNAME:
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# End of file: cs412/settings.py
