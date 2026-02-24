f183fa92e50d570e67be1ebef9864635151900f6
shreyj03 <jainshrey2004@gmail.com>
2026-02-24 15:44:43 -0500

    finished ass5

--- mini_insta/views.py
+++ mini_insta/views.py
@@ -108,3 +108,83 @@ class UpdatePostView(UpdateView):
     def get_success_url(self):
         '''redirect to the updated post's detail page.'''
         return reverse("show_post", kwargs={"pk": self.object.pk})
+    
+class ShowFollowersDetailView(DetailView):
+    '''a view to handle displaying followers'''
+    model = Profile
+    template_name = "mini_insta/show_followers.html"
+    context_object_name = "profile"
+
+    def get_context_data(self,  **kwargs):
+        '''override the built in get_context_data to populate fields.'''
+        context = super().get_context_data(**kwargs)
+        profile = self.get_object()
+        context['followers'] = profile.get_followers()
+        context['num_followers'] = profile.get_num_followers()
+        return context
+    
+class ShowFollowingDetailView(DetailView):
+    '''a view to handle displaying following'''
+    model = Profile
+    template_name = "mini_insta/show_following.html"
+    context_object_name = "profile"
+
+    def get_context_data(self,  **kwargs):
+        '''override the built in get_context_data to populate fields.'''
+        context = super().get_context_data(**kwargs)
+        profile = self.get_object()
+        context["following"] = profile.get_following()
+        context["num_following"] = profile.get_num_following()
+        return context
+
+class PostFeedListView(ListView):
+    '''A view to handle displaying the post feed of a given profile.'''
+    model = Post
+    template_name = "mini_insta/show_feed.html"
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        '''Return the posts in the feed for this profile.'''
+        profile = Profile.objects.get(pk=self.kwargs['pk'])
+        return profile.get_post_feed()
+
+    def get_context_data(self, **kwargs):
+        '''Add the current profile to the context.'''
+        context = super().get_context_data(**kwargs)
+        context['profile'] = Profile.objects.get(pk=self.kwargs['pk'])
+        return context
+    
+class SearchView(ListView):
+    '''a view to handle searching for profiles and posts'''
+    model = Profile
+    template_name = "mini_insta/search_results.html"
+    context_object_name = "profiles"
+
+    def dispatch(self, request, *args, **kwargs):
+        '''if no query render template form, otherwise return dispatch'''
+        if "q" not in self.request.GET:
+            profile = Profile.objects.get(pk=self.kwargs['pk'])
+            return render(request, "mini_insta/search.html", {"profile": profile})
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_queryset(self):
+        '''return posts which match the query'''
+        query = self.request.GET.get("q")
+        if query:
+            return Post.objects.filter(caption__icontains=query)
+        return Post.objects.none()
+
+    def get_context_data(self, **kwargs):
+        '''add profile, query, posts, and matching profiles to context'''
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get("q")
+        profile = Profile.objects.get(pk=self.kwargs['pk'])
+        posts = self.get_queryset()
+        matching_profiles = Profile.objects.filter(username__icontains=query) | \
+                            Profile.objects.filter(display_name__icontains=query) | \
+                            Profile.objects.filter(bio_text__icontains=query)
+        context["profile"] = profile
+        context["query"] = query
+        context["posts"] = posts
+        context["profiles"] = matching_profiles
+        return context
